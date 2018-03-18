522619d27aedf5df91382da0c37ad8e06e05398b
Alena <chestnaja_alena@mail.ru>
2018-03-19 00:15:24 +0300

    Реализовала переключение вкладок в хедере

--- source/js/catalogue-filter.js
+++ source/js/catalogue-filter.js
@@ -1,15 +1,23 @@
-$(function() {
-  var grid = $(".articles").isotope({});
+// $(function() {
+//   var grid = $(".articles").isotope({});
+//   $(".articles__item").css("position", "static");
+//   var buttons = $(".catalogue__nav-button");
+//   buttons.on("click", function() {
+//     var button = $(this);
+//     buttons.removeClass("catalogue__nav-button--current");
+//     button.addClass("catalogue__nav-button--current");
+//     var filterValue = button.attr("data-filter");
+//     grid.isotope({
+//       filter: filterValue
+//     });
+//   })
+// });
 
+$(function() {
   var buttons = $(".catalogue__nav-button");
   buttons.on("click", function() {
     var button = $(this);
     buttons.removeClass("catalogue__nav-button--current");
     button.addClass("catalogue__nav-button--current");
-    var filterValue = button.attr("data-filter");
-    grid.isotope({
-      filter: filterValue
-    });
   })
 });
-
