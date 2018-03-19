d7de7700acf6cbeb1eee2a26325f8459b5c25ed2
Alena <chestnaja_alena@mail.ru>
2018-03-19 09:55:24 +0300

    Переделала фильтр в каталоге

--- source/js/catalogue-filter.js
+++ source/js/catalogue-filter.js
@@ -13,11 +13,30 @@
 //   })
 // });
 
+var filteredClass = ".articles__item";
+var filterButtonClass = ".catalogue__nav-button";
+var showAllButtonClass = ".catalogue__nav-button--all";
+
 $(function() {
-  var buttons = $(".catalogue__nav-button");
+  var buttons = $(filterButtonClass);
   buttons.on("click", function() {
     var button = $(this);
     buttons.removeClass("catalogue__nav-button--current");
     button.addClass("catalogue__nav-button--current");
+    if (button.hasClass(showAllButtonClass)) {
+      showAll();
+    } else {
+      var filterValue = button.attr("data-filter");
+      makeFilter(filterValue);
+    }
   })
 });
+
+function showAll() {
+  $(filteredClass).fadeIn(200);
+};
+
+function makeFilter(className) {
+  $(filteredClass).not(className).fadeOut(200);
+  $(filteredClass + className).delay(200).fadeIn(200);
+};
