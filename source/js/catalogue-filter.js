5da9108599d62bca79f6adf99027b406196b365e
Alena <chestnaja_alena@mail.ru>
2018-03-19 10:21:24 +0300

    Поправила отображение карточек товаров

--- source/js/catalogue-filter.js
+++ source/js/catalogue-filter.js
@@ -1,18 +1,3 @@
-// $(function() {
-//   var grid = $(".articles").isotope({});
-//   $(".articles__item").css("position", "static");
-//   var buttons = $(".catalogue__nav-button");
-//   buttons.on("click", function() {
-//     var button = $(this);
-//     buttons.removeClass("catalogue__nav-button--current");
-//     button.addClass("catalogue__nav-button--current");
-//     var filterValue = button.attr("data-filter");
-//     grid.isotope({
-//       filter: filterValue
-//     });
-//   })
-// });
-
 var filteredClass = ".articles__item";
 var filterButtonClass = ".catalogue__nav-button";
 var showAllButtonClass = ".catalogue__nav-button--all";
